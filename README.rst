8a5a8b6919884e2f0c91157e3f06661dc98b7d6e
Chris Liechti <cliechti@gmx.net>
2016-02-21 01:18:53 +0100

    add python distribution creation tool, docs

--- README.rst
+++ README.rst
@@ -2,6 +2,8 @@
  python-embedded-launcher
 ==========================
 
+For Windows only.
+
 The launcher is a small C program that loads the Python DLL and calls
 ``Py_Main`` with itself as parameter, loading a zipped Python application
 appended to the exe.
@@ -17,5 +19,28 @@ Distribute an application
 Common python-minimal package
     Multiple tools can use a common copy of Python. e.g. with a package manager
     Python can be provided as one package and separate application packages
-    can use that python to run.
+    can use that Python to run.
     => The launcher has the path to the Python installation configurable.
+
+
+Build
+=====
+Requirements
+------------
+mingw(-64) GCC compiler, e.g. http://tdm-gcc.tdragon.net/ has one.
+
+for Python 2.7
+--------------
+In the ``python27`` directory:
+
+- run ``create_python_minimal.py`` to create a python27-minimal distribution
+- run ``compile.bat`` to create ``launcher27.exe`` the ``ziptool.py`` will
+  be used to append ``bootstrap27.py`` (zipped) to the exe.
+
+
+Ideas
+=====
+The launcher should be customizable. To not require an external file or a
+compiler, use Windows resource in the exe. There are resource editor tools
+that can be used to change these.
+
---
+++ python27/create_python_minimal.py
@@ -0,0 +1,50 @@
+"""\
+Create a python-minimal distribution.
+
+Start this tool with Python 2.7. It requires that it is installed to copy the
+files from there.
+
+virtualenv could probably also be used to do this job...
+
+You do not need this for Python 3.5, just download the embedded
+distribution from the official site instead!
+"""
+import sys
+import os
+import shutil
+import zipfile
+
+python_source = os.path.dirname(sys.executable)
+python_destination = 'python27-minimal'
+if not os.path.exists(python_destination):
+    os.mkdir(python_destination)
+
+for name in ('python.exe', 'pythonw.exe', 'w9xpopen.exe'):
+    shutil.copy2(os.path.join(python_source, name),
+                 os.path.join(python_destination, name))
+
+DLL_EXCLUDES = ('tcl85.dll', 'tclpip85.dll', 'tk85.dll', '_tkinter.pyd')
+for name in os.listdir(os.path.join(python_source, 'DLLs')):
+    if name not in DLL_EXCLUDES:
+        shutil.copy2(os.path.join(python_source, 'DLLs', name),
+                     os.path.join(python_destination, name))
+
+# for some reason, this file is somewhere else...
+shutil.copy2(os.path.expandvars('%WINDIR%\\System32\\python27.dll'),
+             os.path.join(python_destination, 'python27.dll'))
+
+# zip the standard libarary (no site-packages and no TCL/TK)
+EXCLUDE_DIRS = ('lib-tk', 'site-packages', 'test')
+with zipfile.ZipFile(os.path.join(python_destination, 'python27.zip'), 'w') as archive:
+    zip_root = os.path.join(python_source, 'Lib')
+    for root, dirs, files in os.walk(zip_root):
+        for dir in EXCLUDE_DIRS:
+            if dir in dirs:
+                dirs.remove(dir)
+        for name in files:
+            filename = os.path.join(root, name)
+            archive.write(
+                    filename,
+                    filename[len(os.path.commonprefix([zip_root, filename])):],
+                    compress_type=zipfile.ZIP_DEFLATED
+                    )
