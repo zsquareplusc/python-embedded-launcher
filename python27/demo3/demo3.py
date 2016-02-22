e517f08fb45960ea57ebb369adaa746b79da8cb5
Chris Liechti <cliechti@gmx.net>
2016-02-22 12:50:16 +0100

    move wx folder one up, easier to add path

--- python27/demo3/demo3.py
+++ python27/demo3/demo3.py
@@ -1,12 +1,7 @@
-# first configure the launcher: restore command line
+# first configure the launcher: restore command line and add exe dir to sys.path
 import launcher
-#~ launcher.patch_sys_path()
+launcher.patch_sys_path()
 launcher.restore_sys_argv()
-
-# add the directory containing wx to the path
-import sys
-import os
-sys.path.append(os.path.join(os.path.dirname(sys.executable), 'wx-3.0-msw'))
 
 import sample_application
 sample_application.main()
