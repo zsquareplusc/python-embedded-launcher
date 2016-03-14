674e661098db589ce65d56b62c55c91ded4db6f8
Chris Liechti <cliechti@gmx.net>
2016-03-14 01:44:31 +0100

    refactor: command line groups, API
    - provide core functionality as function (API)
    - add groups to command line interface

--- launcher_tool/download_python3_minimal.py
+++ launcher_tool/download_python3_minimal.py
@@ -25,27 +25,59 @@ URL_32 = 'https://www.python.org/ftp/python/3.5.1/python-3.5.1-embed-win32.zip'
 URL_64 = 'https://www.python.org/ftp/python/3.5.1/python-3.5.1-embed-amd64.zip'
 
 
+def extract(url, destination, force_download=False):
+    """\
+    extract zip file from cache, download if needed.
+    e.g. extract(URL_32, 'python3-minimal')
+    """
+
+    # where to store
+    cache_dir = os.path.abspath(os.path.expandvars('%LOCALAPPDATA%/python-embedded-launcher/cache'))  # XXX windows only
+    cache_name = os.path.join(cache_dir, re.sub(r'[^\w]', '', url))
+    if not os.path.exists(cache_dir):
+        os.makedirs(cache_dir)
+    # download
+    if not os.path.exists(cache_name) or force_download:
+        sys.stderr.write('downloading {} to {}\n'.format(url, cache_name))
+        response = requests.get(url, stream=True)
+        with open(cache_name, 'wb') as downloaded_data:
+            for chunk in response.iter_content(2**20):
+                downloaded_data.write(chunk)
+    else:
+        sys.stderr.write('using cached file {}\n'.format(cache_name))
+
+    if not os.path.exists(cache_name):
+        sys.stderr.write('failed to download/load from cache: file not found: {}\n'.format(cache_name))
+        raise IOError('file not found: {!r}'.format(cache_name))
+
+    sys.stderr.write('extracting to {}\n'.format(destination))
+    with zipfile.ZipFile(cache_name) as archive:
+        archive.extractall(destination)
+
+
 def main():
     """Console application entry point"""
     parser = argparse.ArgumentParser(description='Launcher assembler')
 
-    parser.add_argument('-d', '--directory', metavar='DIR', default='.',
-                        help='Set a destination directory [default: %(default)s]')
-    parser.add_argument('-n', '--name', metavar='DIR', default='python3-minimal',
-                        help='Set a directory name [default: %(default)s]')
-    parser.add_argument('--url', help='override download URL')
-    parser.add_argument('--32', dest='bits32', action='store_true', default=False,
-                        help='force download of 32 bit version')
-    parser.add_argument('--64', dest='bits64', action='store_true', default=False,
-                        help='force download of 64 bit version')
-    parser.add_argument('-f', '--force-download', action='store_true', default=False,
-                        help='force download (ignore/overwrite cached file)')
+    group_out = parser.add_argument_group('output options')
+    group_out.add_argument('-d', '--directory', metavar='DIR', default='.',
+                           help='set a destination directory, a subdirectory NAME will be creted [default: %(default)s]')
+    group_out.add_argument('-n', '--name', metavar='NAME', default='python3-minimal',
+                           help='set a directory name [default: %(default)s]')
+
+    group_download = parser.add_argument_group('download options')
+    group_url = group_download.add_mutually_exclusive_group()
+    group_url.add_argument('--32', dest='bits32', action='store_true', default=False,
+                           help='force download of 32 bit version')
+    group_url.add_argument('--64', dest='bits64', action='store_true', default=False,
+                           help='force download of 64 bit version')
+    group_url.add_argument('--url', help='override download URL')
+    group_download.add_argument('-f', '--force-download', action='store_true', default=False,
+                                help='force download (ignore/overwrite cached file)')
 
     args = parser.parse_args()
 
     #~ print args
-    if args.url and (args.bits32 or args.bits64):
-        parser.error('--url conflicts with --32 and --64')
 
     if args.url is None:
         if args.bits32:
@@ -58,29 +90,7 @@ def main():
             args.url = URL_32
 
     python_destination = os.path.join(args.directory, args.name)
-
-    # where to store
-    cache_dir = os.path.abspath(os.path.expandvars('%LOCALAPPDATA%/python-embedded-launcher/cache'))  # XXX windows only
-    cache_name = os.path.join(cache_dir, re.sub(r'[^\w]', '', args.url))
-    if not os.path.exists(cache_dir):
-        os.makedirs(cache_dir)
-    # download
-    if not os.path.exists(cache_name) or args.force_download:
-        sys.stderr.write('downloading {} to {}\n'.format(args.url, cache_name))
-        response = requests.get(args.url, stream=True)
-        with open(cache_name, 'wb') as downloaded_data:
-            for chunk in response.iter_content(2**20):
-                downloaded_data.write(chunk)
-    else:
-        sys.stderr.write('using cached file {}\n'.format(cache_name))
-
-    if not os.path.exists(cache_name):
-        sys.stderr.write('failed to download/load from cache: file not found: {}\n'.format(cache_name))
-        sys.exit(1)
-
-    sys.stderr.write('extracting to {}\n'.format(python_destination))
-    with zipfile.ZipFile(cache_name) as archive:
-        archive.extractall(python_destination)
+    extract(args.url, python_destination, args.force_download)
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 if __name__ == '__main__':
