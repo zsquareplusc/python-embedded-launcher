1bb459c2db4b50e0eb059888c00b7d4059ec3e15
Chris Liechti <cliechti@gmx.net>
2016-03-28 23:56:03 +0200

    shrink binaries

--- src/python27/compile.bat
+++ src/python27/compile.bat
@@ -2,3 +2,4 @@
 @for /f %%i in ('python -c "import sys,os; print(os.path.dirname(sys.executable))"') do set PY2_INC=%%i
 windres -F pe-i386 launcher27.rc -O coff -o launcher27.res
 gcc -m32 -o ..\..\launcher_tool\launcher27.exe launcher27.c launcher27.res -Wall -I %PY2_INC%\\include
+strip ..\..\launcher_tool\launcher27.exe
--- src/python3/compile.bat
+++ src/python3/compile.bat
@@ -4,3 +4,4 @@ windres -F pe-i386 launcher3.rc -O coff -o launcher3-32.res
 windres -F pe-x86-64 launcher3.rc -O coff -o launcher3-64.res
 gcc -m32 -o ..\..\launcher_tool\launcher3-32.exe launcher3.c launcher3-32.res -DUNICODE -Wall -I %PY3_INC%\\include
 gcc -m64 -o ..\..\launcher_tool\launcher3-64.exe launcher3.c launcher3-64.res -DUNICODE -Wall -I %PY3_INC%\\include
+strip ..\..\launcher_tool\launcher3-32.exe ..\..\launcher_tool\launcher3-64.exe
