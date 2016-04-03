41a55a295bb59ee774806ba75178c1ca21417260
Chris Liechti <cliechti@gmx.net>
2016-04-04 01:05:02 +0200

    bdist_launcher: support for 32/64 bits and Python 2.7

--- launcher_tool/bdist_launcher.py
+++ launcher_tool/bdist_launcher.py
@@ -1,4 +1,9 @@
 #!python
+#
+# This file is part of https://github.com/zsquareplusc/python-embedded-launcher
+# (C) 2016 Chris Liechti <cliechti@gmx.net>
+#
+# SPDX-License-Identifier:    BSD-3-Clause
 """\
 A distutils/setuptools command extension, so that building applications is as
 easy as "python setup.py bdist_launcher".
@@ -8,20 +13,22 @@ import distutils.cmd
 from distutils import log
 import launcher_tool.copy_launcher
 import launcher_tool.launcher_zip
+import launcher_tool.create_python27_minimal
 import launcher_tool.download_python3_minimal
+from launcher_tool.download_python3_minimal import URL_32, URL_64
 import sys
 import os
 import pkgutil
 import zipfile
 
 
-def write_launcher(filename, main_script):
+def write_launcher(filename, main_script, use_py2, use_64bits):
     """\
     helper function that writes a launcher exe with the appended __main__.py
     and support files
     """
     with open(filename, 'wb') as exe:
-        launcher_tool.copy_launcher.copy_launcher(exe)
+        launcher_tool.copy_launcher.copy_launcher(exe, use_py2, use_64bits)
         with zipfile.ZipFile(exe, 'a', compression=zipfile.ZIP_DEFLATED) as archive:
             archive.writestr('__main__.py', main_script.encode('utf-8'))
             archive.writestr('launcher.py', pkgutil.get_data('launcher_tool', 'launcher.py'))
@@ -33,6 +40,7 @@ class LauncherCommand(distutils.cmd.Command):
     """
 
     description = "Build windows executables"
+
     user_options = []
 
     def initialize_options(self):
@@ -45,7 +53,12 @@ class LauncherCommand(distutils.cmd.Command):
         #~ print(dir(self.distribution))
         #~ print(self.distribution.scripts)
         #~ print(self.distribution.requires)
-        dest_dir = os.path.join('dist', 'launcher')
+        use_python27 = (sys.version_info.major == 2)
+        is_64bits = sys.maxsize > 2**32  # recommended by docs.python.org "platform" module
+
+        dest_dir = os.path.join('dist', 'launcher{}-{}'.format(
+            '27' if use_python27 else '3',
+            '64' if is_64bits else '32'))
         log.info('installing to {}'.format(dest_dir))
         self.mkpath(dest_dir)
         
@@ -68,23 +81,31 @@ class LauncherCommand(distutils.cmd.Command):
                     filename = os.path.join(dest_dir, '{}.exe'.format(name))
                     self.execute(write_launcher,
                                  (filename,
-                                  launcher_tool.launcher_zip.make_main(entry_point=entry_point)),
+                                  launcher_tool.launcher_zip.make_main(entry_point=entry_point),
+                                  use_python27, is_64bits),
                                  'writing launcher {}'.format(filename))
 
         if hasattr(self.distribution, 'scripts'):
             for source in self.distribution.scripts:
                 filename = os.path.join(dest_dir, '{}.exe'.format(os.path.basename(source)))
                 script = open(source).read()
-                # append script to launcher boot code
+                # append users' script to the launcher boot code
                 main_script = '{}\n{}'.format(launcher_tool.launcher_zip.make_main(), script)
                 self.execute(write_launcher,
-                             (filename, main_script),
+                             (filename, main_script, use_python27, is_64bits),
                              'writing launcher {}'.format(filename))
 
-        if not os.path.exists(os.path.join(dest_dir, 'python3-minimal')):
-            log.info('extracting python minimal installation')
-            launcher_tool.download_python3_minimal.extract(
-                launcher_tool.download_python3_minimal.URL_32,
-                os.path.join(dest_dir, 'python3-minimal'))
+        if use_python27:
+            if not os.path.exists(os.path.join(dest_dir, 'python27-minimal')):
+                launcher_tool.create_python27_minimal.copy_python(
+                    os.path.join(dest_dir, 'python27-minimal'))
+            else:
+                log.info('python27-minimal installation already present')
         else:
-            log.info('python minimal installation already present')
+            if not os.path.exists(os.path.join(dest_dir, 'python3-minimal')):
+                log.info('extracting python minimal installation')
+                launcher_tool.download_python3_minimal.extract(
+                    URL_64 if is_64bits else URL_32,
+                    os.path.join(dest_dir, 'python3-minimal'))
+            else:
+                log.info('python3-minimal installation already present')
