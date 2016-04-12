ad47c512689bd46160998dc42b397e4aadcbbcc2
Chris Liechti <cliechti@gmx.net>
2016-04-12 02:13:39 +0200

    copy_wx: optionally skip some big sub directories that are not always needed

--- examples/demo_wx_classic/copy_wx.py
+++ examples/demo_wx_classic/copy_wx.py
@@ -13,7 +13,23 @@ import wx
 wx_source = os.path.dirname(wx.__file__)
 wx_name = os.path.basename(os.path.dirname(wx_source))
 
+# exclude some big libraries, not commonly used in projects
+# note: must use backslash here!
+exclude_dirs = (
+    'tools\\Editra',    # saves ~12MB
+    'tools\\XRCed',     # saves ~1.1MB
+    #~ 'locale',           # saves ~4.7MB but then it won't translate a lot of strings
+    #~ 'lib\\agw',         # saves ~9MB
+    'build',            # saves ~80kB
+)
+
 for root, dirs, files in os.walk(wx_source):
+    for directory in dirs:
+        dirname = os.path.join(root, directory)
+        dirname_rel = dirname[1+len(os.path.commonprefix([wx_source, dirname])):]
+        if dirname_rel in exclude_dirs:
+            sys.stderr.write('skipping {!r}\n'.format(dirname_rel))
+            dirs.remove(directory)
     for name in files:
         filename = os.path.join(root, name)
         dst = os.path.join(sys.argv[1], 'wx', filename[1+len(os.path.commonprefix([wx_source, filename])):])
