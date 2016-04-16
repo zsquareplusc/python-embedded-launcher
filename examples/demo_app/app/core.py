9fa30ca5f060ed76e694f8e7fe106f01377bfdec
Chris Liechti <cliechti@gmx.net>
2016-04-17 00:45:14 +0200

    example: update example to show a message box

--- examples/demo_app/app/core.py
+++ examples/demo_app/app/core.py
@@ -1,11 +1,23 @@
 #!python
+# encoding: utf-8
 """\
 Sample application
 """
 import sys
+import ctypes
 
 def main():
-    sys.stdout.write('parameters: {}\n'.format(sys.argv))
+    try:
+        import launcher
+    except ImportError:
+        pass
+    else:
+        launcher.close_console()
+    ctypes.windll.user32.MessageBoxW(
+        None,
+        u'APP is running!\nParameters:\n{}'.format('\n'.join(sys.argv)),
+        u'demo_app',
+        0x40)
 
 if __name__ == '__main__':
     main()
